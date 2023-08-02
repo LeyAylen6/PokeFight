29881b80104a621c15bab5a88978861d681c9620
Leila <leiisalguero@gmaill.com>
2023-08-02 12:42:06 -0300

    get appearance from api

--- server/src/main/java/com/example/demo/models/APIResponses/getAppearance/GetAppearance.java
+++ server/src/main/java/com/example/demo/models/APIResponses/getAppearance/GetAppearance.java
@@ -1,2 +1,20 @@
-package com.example.demo.models.APIResponses.getAppearance;public class getAppearance {
+package com.example.demo.models.APIResponses.getAppearance;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class GetAppearance {
+
+    @SerializedName("capture_rate")
+    public Integer captureRate;
+
+    @SerializedName("is_legendary")
+    public Boolean isLegendary;
+
+    @SerializedName("is_mythical")
+    public Boolean isMythical;
+
 }
